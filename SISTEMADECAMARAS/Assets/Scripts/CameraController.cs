854a5a301cd128d03e6efe72e03b8cfd4a621abc
Ximena Hidalgo <122484522+XimenaFHH@users.noreply.github.com>
2023-03-12 16:02:01 -0600

    Juego
    Aquí se encuentra todo lo que necesitemos para ver el juego

---
+++ SISTEMADECAMARAS/Assets/Scripts/CameraController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject[] listaCamaras;
+    public Text positionText;
+    public Text rotationText;
+    public Text scaleText;
+
+    private int activeCameraIndex = 0;
+
+    //Tenemos la función ApagarCamaras(); y activando la lsta de cámaras que pusimos 
+    void Start()
+    {
+        ApagarCamaras();
+        listaCamaras[activeCameraIndex].SetActive(true);
+    }
+
+    //Un bucle que apaga las camaras para que estas no esten activas a menos que lo necesitemos.
+    void ApagarCamaras()
+    {
+        foreach (GameObject camara in listaCamaras)
+        {
+            camara.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        //En esta parte hacemos una un if donde marcamos que si le damos click
+        //a una tecla se activara una de las vista de cámara y su respectiva información en pantalla.
+        if (Input.GetKey(KeyCode.Alpha1) && activeCameraIndex != 0)
+        {
+            Debug.Log("Tecla 1 pulsada");
+            ApagarCamaras();
+            activeCameraIndex = 0;
+            listaCamaras[activeCameraIndex].SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2) && activeCameraIndex != 1)
+        {
+            Debug.Log("Tecla 2 pulsada");
+            ApagarCamaras();
+            activeCameraIndex = 1;
+            listaCamaras[activeCameraIndex].SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha3) && activeCameraIndex != 2)
+        {
+            Debug.Log("Tecla 3 pulsada");
+            ApagarCamaras();
+            activeCameraIndex = 2;
+            listaCamaras[activeCameraIndex].SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha4) && activeCameraIndex != 3)
+        {
+            Debug.Log("Tecla 4 pulsada");
+            ApagarCamaras();
+            activeCameraIndex = 3;
+            listaCamaras[activeCameraIndex].SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha5) && activeCameraIndex != 4)
+        {
+            Debug.Log("Tecla 5 pulsada");
+            ApagarCamaras();
+            activeCameraIndex = 4;
+            listaCamaras[activeCameraIndex].SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha6) && activeCameraIndex != 5)
+        {
+            Debug.Log("Tecla 6 pulsada");
+            ApagarCamaras();
+            activeCameraIndex = 5;
+            listaCamaras[activeCameraIndex].SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha7) && activeCameraIndex != 6)
+        {
+            Debug.Log("Tecla 7 pulsada");
+            ApagarCamaras();
+            activeCameraIndex = 6;
+            listaCamaras[activeCameraIndex].SetActive(true);
+        }
+
+        // Activar la camara 
+        Camera activeCamera = listaCamaras[activeCameraIndex].GetComponent<Camera>();
+
+        // Esto nos permite mostrar la información en la pantalla.
+        positionText.text = "Position: " + activeCamera.transform.position.ToString();
+        rotationText.text = "Rotation: " + activeCamera.transform.rotation.eulerAngles.ToString();
+        scaleText.text = "Scale: " + activeCamera.transform.localScale.ToString();
+    }
+}
+
+
---
+++ SISTEMADECAMARAS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 0;
+    public float rotattionSpeed = 0;
+
+    public Animator animator;
+
+    private float x, y;
+
+    public Rigidbody rb;
+    public float JumpHeight = 3;
+
+    public Transform grounCheck;
+    public float grounDistance = 0.1f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * rotattionSpeed, 0);
+
+        transform.Translate(0, 0, y * Time.deltaTime * Speed);
+
+        animator.SetFloat("VeX", x);
+        animator.SetFloat("VeY", y);
+
+        if (Input.GetKey("e"))
+        {
+            animator.SetBool("Other", false);
+
+            animator.Play("Eat");
+            animator.Play("Eyes_Excited");
+        }
+
+        if (Input.GetKey("q"))
+        {
+            animator.SetBool("Other", false);
+            
+            animator.Play("Sit");
+            animator.Play("Eyes_Sleep");
+        }
+
+        if (Input.GetKey("f"))
+        {
+            animator.SetBool("Other", false);
+
+            animator.Play("Death");
+            animator.Play("Eyes_Trauma");
+        }
+
+        if (Input.GetKey("r"))
+        {
+            animator.SetBool("Other", false);
+
+            animator.Play("Spin");
+            animator.Play("Eyes_Squint");
+        }
+
+        if (x>0 || x<0 || y>0 || y<0)
+        {
+            animator.SetBool("Other", true);
+        }
+
+        isGrounded = Physics.CheckSphere(grounCheck.position, grounDistance, groundMask);
+
+        if(Input.GetKey("space") && isGrounded)
+        {
+            animator.Play("Jump");
+           
+
+            rb.AddForce(Vector3.up * JumpHeight, ForceMode.Impulse);
+        }
+    }
+}
